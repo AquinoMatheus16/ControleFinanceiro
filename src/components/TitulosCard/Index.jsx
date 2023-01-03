1ac4766135d1ff00eab1adf57cac5a2015d1b3ea
AquinoMatheus16 <matheus.aquino1.618@gmail.com>
2023-01-03 16:18:57 -0300

    Títulos a pagar

---
+++ src/components/TitulosCard/Index.jsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+import { styles } from "./styles";
+
+export const TitulosCard = ({ item }) => {
+
+    return (
+        <View style={styles.containerMain}>
+            <Text style={styles.texto}>GVUGYUYH</Text>
+        </View>
+    )
+};
--- src/screens/TitulosApagar/index.jsx
+++ src/screens/TitulosApagar/index.jsx
@@ -1,8 +1,22 @@
-import { Text } from "react-native";
+import { Text, View } from "react-native";
+import { styles } from "./styles";
+import { TitulosCard } from "../../components/TitulosCard/Index";
 
 export const TitulosApagar = () => {
 
     return (
-        <Text>Titulos a pagar</Text>
+        <View style={styles.containerMian}>
+            <Text style={styles.textoTotal}>Total a pagar</Text>
+            <Text style={styles.textoNumero}>1900.00</Text>
+
+            <Text style={styles.textoTitulo}>Títulos a pagar</Text>
+
+            <View style={styles.containerCard}>
+                <TitulosCard />
+                <TitulosCard />
+                <TitulosCard />
+                <TitulosCard />
+            </View>
+        </View>
     )
-}
+};
