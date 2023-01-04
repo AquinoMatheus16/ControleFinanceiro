53a6773b72191ab1120179d61717a0d8f1e85787
andersonlps <andersomlps@gmail.com>
2023-01-04 20:53:15 -0300

    Foto usuário

--- src/screens/Conta/index.jsx
+++ src/screens/Conta/index.jsx
@@ -1,20 +1,39 @@
-import { useContext } from "react";
-import { Button, Text, TouchableOpacity, View } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage"
+import { async } from "q";
+import { useContext, useState } from "react";
+import { Button, Image, Text, TouchableOpacity, View } from "react-native";
+import { Source } from "webpack-sources";
 import { AuthContext } from "../../contexts/AuthContext";
 import { styles } from "./styles";
 
 export const Conta = () => {
     const { logoutContext } = useContext(AuthContext);
+    const [fot, setFot] = useState("");
+
+    const img = async ()  => {
+        const data = await AsyncStorage.getItem("@app_user");
+        const usuario = JSON.parse(data);
+        setFot(usuario.foto)
+        return usuario.id;
+    }
+    img();
+
     return (
-        <>
-        <Text>Conta</Text>
-        <TouchableOpacity
-                onPress={() => logoutContext()}>
-            <View style={styles.botaoEntrar}>
-            <Text style={styles.entrar}>Sair</Text>
+        <View style={styles.containerPrincipal}>
+
+            <Text>Conta</Text>
+
+            <View >
+               <Image source={ {uri: fot ? fot : null}} style={styles.homeDashboard}/>
             </View>
-        </TouchableOpacity>
-        
-        </>
+
+            <TouchableOpacity
+                onPress={() => logoutContext()}>
+                <View style={styles.botaoEntrar}>
+                    <Text style={styles.entrar}>Sair</Text>
+                </View>
+            </TouchableOpacity>
+
+        </View>
     )
 }
