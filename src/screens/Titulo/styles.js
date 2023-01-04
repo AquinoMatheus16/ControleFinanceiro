8baca9edcb168c89784cdb9b29be0ede0c960724
AquinoMatheus16 <matheus.aquino1.618@gmail.com>
2023-01-04 20:59:18 -0300

    Título Cadastro

--- src/screens/Titulo/styles.js
+++ src/screens/Titulo/styles.js
@@ -17,5 +17,45 @@ export const styles = StyleSheet.create({
         color: '#FFFFFF',
         borderBottomWidth: 1,
         borderBottomColor: '#FFFFFF'
+    },
+
+    touchableOpacity: {
+        width: 190,
+        height: 45,
+        backgroundColor: '#FFFFFF',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 10,
+        marginTop: 25
+    },
+
+    touchableOpacityTexto: {
+        fontSize: 22
+    },
+
+    containerInput: {
+        width: '80%',
+        flexDirection: 'row',
+        borderBottomWidth: 1,
+        borderBottomColor: '#FFFFFF',
+        alignItems: 'center',
+        marginBottom: 5
+    },
+
+    textoTituloInput: {
+        color: '#FFFFFF',
+        fontSize: 22,
+        marginTop: 50
+    },
+
+    textInput: {
+        width: '100%',
+        padding: 10,
+        color: '#FFFFFF',
+    },
+
+    containerFlatList: {
+        marginBottom: 300
     }
+
 });
---
+++ src/screens/TituloCadastra/styles.js
@@ -0,0 +1,36 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+
+    scrollView: {
+        flex: 1,
+        backgroundColor: '#0C609C',
+    },
+
+    containerMain: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingTop: 20
+    },
+
+    // containerInput: {
+    //     width: '80%',
+    //     alignItems: 'center',
+    //     padding: 10,
+    //     borderRadius: 10
+    // },
+
+    textInput: {
+        borderBottomWidth: 1,
+        borderBottomColor: '#FFFFFF',
+        padding: 5,
+        width: '80%',
+        marginBottom: 20
+    },
+
+    texto: {
+        fontSize: 22,
+        color: '#FFFFFF'
+    }
+
+});
