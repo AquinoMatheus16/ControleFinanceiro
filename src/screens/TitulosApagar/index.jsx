8527c03e42543848f1f3180c49a2c36f95709467
andersonlps <andersomlps@gmail.com>
2023-01-05 21:57:02 -0300

    Titulo areceber e saldo

--- src/screens/TitulosApagar/index.jsx
+++ src/screens/TitulosApagar/index.jsx
@@ -2,11 +2,12 @@ import { FlatList, Text, View } from "react-native";
 import { styles } from "./styles";
 import { TitulosCard } from "../../components/TitulosCard";
 import { useEffect, useState } from "react";
-import { getTitulo } from "../../services/titulo";
+import { getDashBoardTotal, getTitulo } from "../../services/titulo";
 
 export const TitulosApagar = () => {
 
     const [titulos, setTitulos] = useState([]);
+    const [total, setTotal] = useState("");
 
     const fetchData = async () => {
 
@@ -15,21 +16,31 @@ export const TitulosApagar = () => {
 
     };
 
+    const fetchTotal = async () => {
+
+        const total = await getDashBoardTotal();
+        setTotal(total);
+
+    };
+
     useEffect(() => {
         fetchData();
+        fetchTotal();
     }, []);
 
     return (
         <>
             <View style={styles.containerMian}>
                 <Text style={styles.textoTotal}>Total a pagar</Text>
-                <Text style={styles.textoNumero}>00.00</Text>
+                <Text style={styles.textoNumero}>R$: {total.totalApagar}</Text>
 
                 <Text style={styles.textoTitulo}>Títulos a pagar</Text>
 
                 <View style={styles.containerCard}>
                     <FlatList
-                        data={titulos}
+                        data={titulos.filter((titulo) =>
+                            titulo.tipo.endsWith("APAGAR")
+                        )}
                         keyExtractor={item => item.id}
                         renderItem={({ item }) => <TitulosCard item={item} />}
                     />
