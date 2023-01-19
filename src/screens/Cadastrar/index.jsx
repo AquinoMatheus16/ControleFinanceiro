eada7fbc440254238bae70b8ed85b20ffecd400d
AquinoMatheus16 <matheus.aquino1.618@gmail.com>
2023-01-19 16:03:28 -0300

    Validações dos inputs da tela de cadastro

--- src/screens/Cadastrar/index.jsx
+++ src/screens/Cadastrar/index.jsx
@@ -1,24 +1,40 @@
 import { useState } from 'react';
-import { Text, View, TextInput, TouchableOpacity, Image } from 'react-native';
+import { Text, View, TouchableOpacity, Image } from 'react-native';
 import { styles } from './styles';
 import * as ImagePicker from 'expo-image-picker';
 import { postUsuario } from '../../services/usuario';
 import { InputGeral } from '../../components/InputGeral';
+import { EvilIcons } from '@expo/vector-icons';
+import { useForm, Controller } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+import { ModalSuccessful } from '../../components/ModalSuccessful';
+
+const schema = yup.object({
+  nome: yup.string().min(3, "O nome deve ter pelo menos 3 digitos").required("Informe o nome"),
+  email: yup.string().email("E-mail inválido").required("Informe o email"),
+  senha: yup.string().min(4, "A senha deve ter pelo menos 4 digitos").required("Informe a senha"),
+  confirmaSenha: yup.string().oneOf([yup.ref("senha"), null], "Senhas e confirma senha não sao iguais").required("Informe o confirma senha")
+});
 
 export const Cadastrar = ({ navigation }) => {
-  const [nome, setNome] = useState('');
-  const [email, setEmail] = useState('');
-  const [senha, setSenha] = useState('');
-  const [confirmaSenha, setConfirmaSenha] = useState('');
+
   const [image, setImage] = useState(null);
   const [listUsuarios, setListUsuarios] = useState([]);
+  const [mostrarModal, setMostrarModal] = useState(false);
+
+  const { control, handleSubmit, formState: { errors } } = useForm({
+    resolver: yupResolver(schema)
+  });
 
   const pickImage = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
       allowsEditing: true,
       aspect: [4, 3],
       quality: 1,
+      saveToPhotos: true,
+      didCancel: true,
     });
 
     if (!result.canceled) {
@@ -26,94 +42,108 @@ export const Cadastrar = ({ navigation }) => {
     }
   };
 
-  const handleUsuario = async () => {
-    if (nome == "" || email == "" || senha == "" || confirmaSenha == "") {
-      alert("Preencha todos os campos");
-      return;
-    }
-
-    if (senha != confirmaSenha) {
-      alert("Senhas não são iguais");
-      return;
-    }
+  const handleUsuario = async (data) => {
 
     const novoUsuario = {
-      nome: nome,
-      email: email,
-      senha: senha,
-      confirmaSenha: confirmaSenha,
+      nome: data.nome,
+      email: data.email,
+      senha: data.senha,
+      confirmaSenha: data.confirmaSenha,
       foto: image
     };
 
     try {
-      const { data } = await postUsuario(novoUsuario);
-      console.log(data);
+      const { dataa } = await postUsuario(novoUsuario);
+      console.log(dataa);
       setListUsuarios([...listUsuarios, data]);
-      setTimeout(() => {
-        alert("Usuário cadastrado com sucesso")
-      }, 2000);
-      navigation.goBack();
-    } catch (error) {
-      console.log(error)
-    }
 
+      setMostrarModal(true);
+
+      setTimeout(() => {
+        navigation.goBack();
+      }, 2000);
+
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
     <View style={styles.containerPrincipal}>
       <View style={styles.containerLogin}>
 
-        {/* <View style={styles.logoContainer}>
-          <ImageBackground source={logo} style={styles.imagemLogo} />
-        </View> */}
-
-
-        {/* {image ? <Image source={{ uri: image }} style={styles.img} /> : <EvilIcons name="image" size={300} color="black" />} */}
-
         <View style={styles.container2}>
-
           <TouchableOpacity
             style={styles.buttonArquivo}
-            onPress={pickImage}>
-            <Image source={{ uri: image }} style={styles.img} />
+            onPress={pickImage}
+          >
+            {image ? <Image source={{ uri: image }} style={styles.img} /> : <EvilIcons name="user" size={200} style={styles.icon} color="#ffffff" />}
           </TouchableOpacity>
         </View>
 
         <Text style={styles.tituloTexto}>Nome</Text>
-        <InputGeral
-          placeholder={'INSIRA SEU NOME'}
-          onChangeText={setNome}
-          value={nome}
+        <Controller
+          control={control}
+          name="nome"
+          render={({ field: { onChange, value } }) => (
+            <InputGeral
+              placeholder={'INSIRA SEU NOME'}
+              onChangeText={onChange}
+              value={value}
+            />
+          )}
         />
+        {errors.nome && <Text style={styles.textError}>{errors.nome?.message}</Text>}
 
         <Text style={styles.tituloTexto}>E-mail</Text>
-        <InputGeral
-          placeholder={'INSIRA SEU EMAIL'}
-          onChangeText={setEmail}
-          value={email}
+        <Controller
+          control={control}
+          name="email"
+          render={({ field: { onChange, value } }) => (
+            <InputGeral
+              placeholder={'INSIRA SEU EMAIL'}
+              onChangeText={onChange}
+              value={value}
+            />
+          )}
         />
+        {errors.email && <Text style={styles.textError}>{errors.email?.message}</Text>}
 
         <Text style={styles.tituloTexto}>Senha</Text>
-        <InputGeral
-          placeholder={'INSIRA SUA SENHA'}
-          onChangeText={setSenha}
-          value={senha}
-          secureTextEntry={true}
+        <Controller
+          control={control}
+          name="senha"
+          render={({ field: { onChange, value } }) => (
+            <InputGeral
+              placeholder={'INSIRA SUA SENHA'}
+              onChangeText={onChange}
+              value={value}
+              secureTextEntry={true}
+            />
+          )}
         />
-        <Text style={styles.tituloTexto}>Confirma Senha</Text>
-        <InputGeral
-          placeholder={'CONFIRME SUA SENHA'}
-          onChangeText={setConfirmaSenha}
-          value={confirmaSenha}
-          secureTextEntry={true}
-        />
+        {errors.senha && <Text style={styles.textError}>{errors.senha?.message}</Text>}
 
-        <TouchableOpacity
-          onPress={() => handleUsuario()}>
-          <View style={styles.botaoEntrar}>
-            <Text style={styles.entrar}>Cadastrar</Text>
-          </View>
+        <Text style={styles.tituloTexto}>Confirma Senha</Text>
+        <Controller
+          control={control}
+          name="confirmaSenha"
+          render={({ field: { onChange, value } }) => (
+            <InputGeral
+              placeholder={'CONFIRME SUA SENHA'}
+              onChangeText={onChange}
+              value={value}
+              secureTextEntry={true}
+            />
+          )}
+        />
+        {errors.confirmaSenha && <Text style={styles.textError}>{errors.confirmaSenha?.message}</Text>}
+
+        <TouchableOpacity onPress={handleSubmit(handleUsuario)}>
+          <Text style={styles.entrar}>Cadastrar</Text>
         </TouchableOpacity>
+
+        <ModalSuccessful isVisible={mostrarModal} textoModal={"Usuário cadastrado com sucesso!"} />
 
       </View>
     </View>
